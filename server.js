a9d8c20289ad27222ad853026597ecc19aee0d92
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-25 12:25:07 +0530

    request generated

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const app = express();
+app.get("/" , function(request , response){
+    console.log(request);
+});
+
 app.listen(3000 , function(){console.log("server started on port 3000");});
