00f837ce601b3fd503293fd142f7ec6c7305a067
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-25 12:33:22 +0530

    sending html as response

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/" , function(request , response){
-    response.send("HEllo");
+    response.send("<h1>Hello</h1>");
 }); 
 
 app.listen(3000 , function(){console.log("server started on port 3000");});
